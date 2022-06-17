cbc0c429147a33412000ba3000c12d741e138425
muathye <muath.ye@gmail.com>
2022-06-17 07:46:16 +0300

    Update WalletFactory

---
+++ database/factories/WalletFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace YemeniOpenSource\LaravelWallet\Database\Factories;
+
+use YemeniOpenSource\LaravelWallet\Models\Wallet;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WalletFactory extends Factory
+{
+    protected $model = Wallet::class;
+
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
+
