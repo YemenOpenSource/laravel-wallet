3c6909c2003506878190a7a524376f32bc30c35d
muathye <muath.ye@gmail.com>
2022-06-17 21:56:01 +0300

    Update wallet test

--- tests/Unit/WalletTest.php
+++ tests/Unit/WalletTest.php
@@ -29,8 +29,17 @@ class WalletTest extends TestCase
     /** @test */
     function a_wallet_has_a_balance()
     {
+        $rand_balance = mt_rand(11, 999999) / 1000;
+        $wallet = Wallet::factory()->make(['balance' => $rand_balance]);
+
+        $this->assertEquals($rand_balance, $wallet->balance);
+    }
+    
+    /** @test */
+    function a_wallet_balance_is_positive()
+    {
         $wallet = Wallet::factory()->make();
 
-        $this->assertNotNull($wallet->balance);
+        $this->assertGreaterThanOrEqual(0, $wallet->balance);
     }
 }
