cbe27438187f488f366d93faa27bfa25164de031
muathye <muath.ye@gmail.com>
2022-06-17 07:46:47 +0300

    Update TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -19,8 +19,12 @@ class TestCase extends \Orchestra\Testbench\TestCase
     ];
   }
 
-  protected function getEnvironmentSetUp($app)
+  public function getEnvironmentSetUp($app)
   {
-    // perform environment setup
+    // import the CreatePostsTable class from the migration
+    include_once __DIR__ . '/../database/migrations/2021_10_16_043646_create_wallet_tables.php';
+
+    // run the up() method of that migration class
+    (new \CreateWalletTables)->up();
   }
 }
