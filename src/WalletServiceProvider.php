5f775360cc53c53ac1b3ed7837110c3ef11a11c9
muathye <muath.ye@gmail.com>
2022-06-17 05:50:42 +0300

    Add WalletServiceProvider

---
+++ src/WalletServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace YemeniOpenSource\LaravelWallet;
+
+use Illuminate\Support\ServiceProvider;
+
+class WalletServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/config.php' => config_path('wallet.php'),
+            ], 'config');
+
+            $this->publishes([
+                __DIR__ . '/../database/migrations/' => database_path('migrations/'),
+            ], 'migrations');
+        }
+
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'wallet');
+    }
+}
