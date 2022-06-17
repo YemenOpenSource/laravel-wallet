7fbcbd53bbc6a6ba647b572f0d20b957124fdb98
muathye <muath.ye@gmail.com>
2022-06-17 05:35:42 +0300

    Add UpdateWalletTransactionsTable

---
+++ database/migrations/2021_10_24_123456_update_wallet_transactions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateWalletTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $prefix = config('wallet.prefix');
+
+        Schema::table($prefix . 'transactions', function (Blueprint $table) use ($prefix) {
+            $table->integer('origin_id')->unsigned()->nullable();
+            $table->foreign('origin_id')->references('id')->on($prefix . 'transactions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $prefix = config('wallet.prefix');
+
+        if (\DB::getDriverName() !== 'sqlite') {
+            Schema::table($prefix . 'transactions', function (Blueprint $table) {
+                $table->dropForeign(['origin_id']);
+            });
+        }
+
+        Schema::table($prefix . 'transactions', function (Blueprint $table) {
+            $table->dropColumn(['origin_id']);
+        });
+
+    }
+}
